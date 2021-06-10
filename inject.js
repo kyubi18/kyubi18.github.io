b7486e211ec36ba37855a45b4a21f3d05bbe5c06
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-10 15:21:11 +0700

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,46 @@
+let dbAds=[
+  {
+    "data" :``
+  }
+];
+
+dbAds.forEach(function(a){
+  let createElDom=document.createElement("div");
+  let dataScript=[];
+  createElDom.querySelectorAll("script").forEach(function(b){
+    let createElCostom=document.createElement("script");
+    createElCostom.innerHTML=b.innerHTML;
+    dataScript.push(createElCostom);
+    b.remove();
+  });
+});
+
+
+let elImg=document.querySelectorAll("img");
+elImg.forEach(function(a){
+  a.setAttribute("style","max-width:100%");
+  let classImg=a.getAttribute("class");
+  let getSrcSet=a.getAttribute("srcset");
+  if(classImg==null==false){
+    a.classList.remove("lazyload");
+  };
+  if(getSrcSet==null==false){
+    getSrcSet=getSrcSet.split(",");
+    if(getSrcSet.length>1){
+      a.setAttribute("src",getSrcSet);
+    };
+  };
+});
+
+let dataLazy=document.querySelectorAll(".lazy-image.lazy-image-udf");
+dataLazy.forEach(function(a){
+  let dataHref=a.getAttribute("data-src");
+  if(dataHref){
+    let targetLazy=a.querySelector(".loadingPlaceholder");
+    let targetDiv=a.querySelector(".lazy-image__loadingPlaceholder")
+    if(targetLazy){
+      targetLazy.setAttribute("src",dataHref);
+      targetDiv.setAttribute("class","show")
+    };
+  };
+});
