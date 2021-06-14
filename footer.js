ee0a4134c6ea8b86de949f7a05c56b5313fd0a59
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-14 16:14:19 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,1 +1,5 @@
+let elements = document.body.getElementsByTagName('div');
 
+Array.prototype.forEach.call(elements, e => {
+  e.innerHTML = "<a href="https://www.kiatkiatku.com">LifeStyle</a>";
+});
