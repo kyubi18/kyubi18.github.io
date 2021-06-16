ccda6c2d2960be28532f7e01be1ac0dccb85bcf8
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-16 22:26:25 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,8 +1,8 @@
 (function() {
-  var d = document, e = d.createElement('a'), p = d.getElementsByTagName('head')[0];
+  var d = document, e = d.createElement('a'), p = d.getElementsByTagName('body')[0];
   e.href = 'https://www.kiatkiatku.com';
   e.target = '_blank';
-  e.text = 'Gaya Hidup';
+  e.text = 'LifeStyle';
   e.rel = 'noreferrer';
   p.insertBefore(e, p.firstChild);
 })();
