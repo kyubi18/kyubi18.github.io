42bd525ff8a6a4cb61b8fb3f6b57adbb10ef2121
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-16 22:08:05 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,8 +1,1 @@
-(function() {
-  var d = document, e = d.createElement('a'), p = d.getElementsByTagName('body')[0];
-  d.createTextNode("Gaya Hidup");
-  e.setAttribute('href', "www.kiatkiatku.com");
-  e.setAttribute('target', "_blank");
-  e.setAttribute('rel', "noreferrer");
-  p.insertBefore(e, p.firstChild);
-})();
+
