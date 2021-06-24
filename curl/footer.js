274fb02b5d29ae659f2be07ccdf7089aa0f219ed
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-24 14:40:41 +0700

    Update footer.js

--- curl/footer.js
+++ curl/footer.js
@@ -9,7 +9,7 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
   r.insertBefore(e, r.firstChild);
 })();
 
-var el=["channel-live-chat","channel-tanya-dokter","channel-download-aplikasi"];
+var el=["channel-live-chat","channel-tanya-dokter","channel-download-aplikasi","channel-belanja-sehat"];
 for (i=0;i<el.length;i++) {
     document.getElementById(el[i]).remove();
 }
