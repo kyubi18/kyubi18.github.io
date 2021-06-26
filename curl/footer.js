c37c0ab48b8a54779ef0d6afffbf6695754b71cf
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-26 18:26:04 +0700

    Update footer.js

--- curl/footer.js
+++ curl/footer.js
@@ -4,7 +4,8 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
   var d = document, e = d.createElement('a'), p = d.getElementsByTagName('p'), r = p[random(0, (p.length - 1))];
   e.href = 'https://www.kiatkiatku.com';
   e.target = '_blank';
-  e.text = 'SPONSORED: LifeStyle ';
+  e.title = 'Lifestyle';
+  e.text = 'LifeStyle ';
   e.rel = 'sponsored';
   r.insertBefore(e, r.firstChild);
 })();
