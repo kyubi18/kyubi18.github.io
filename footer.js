db187aea8716a0a291b8d1682ff5f15d9baf04a6
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-26 19:41:38 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,9 +2,12 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
 
 (function() {
   var d = document, e = d.createElement('a'), p = d.getElementsByTagName('p'), r = p[random(0, (p.length - 1))];
-  e.href = 'https://www.kiatkiatku.com';
-  e.target = '_blank';
-  e.text = 'LifeStyle ';
-  e.rel = 'noreferrer';
-  r.insertBefore(e, r.firstChild);
+  //e.href = 'https://www.kiatkiatku.com';
+  //e.target = '_blank';
+  //e.title = 'Lifestyle';
+  //e.text = 'LifeStyle ';
+  //e.rel = 'sponsored';
+  //r.insertBefore(e, r.firstChild);
+  
+  r.innerHTML += '<br/><br/>Sponsored: <a href="https://www.kiatkiatku.com" target="_blank" title="Lifestyle" rel="sponsored">LifeStyle</a>';
 })();
