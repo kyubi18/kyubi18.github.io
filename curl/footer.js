d59438df6d56f6883dbc4d4ec169d3f8b2eee37b
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-06-27 12:43:42 +0700

    Update footer.js

--- curl/footer.js
+++ curl/footer.js
@@ -22,5 +22,9 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
   document.getElementById("M591273ScriptRootC918445_02264").remove();
   document.getElementById("ssp_doubleclick_02264").remove();
   document.querySelectorAll(".mgbox").remove();
+  document.querySelectorAll("mgbox").remove();
+  document.querySelector(".mgbox").remove();
+  document.querySelector("mgbox").remove();
+
 
 })();
