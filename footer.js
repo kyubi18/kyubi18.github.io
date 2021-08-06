c65335624ec152df0fedea6ded90300d4e9defb0
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-08-06 20:53:24 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -9,5 +9,5 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
   //e.rel = 'sponsored';
   //r.insertBefore(e, r.firstChild);
   
-  r.innerHTML += '<br/><br/>Sponsored: <a href="https://www.kiatkiatku.com" target="_blank" title="Lifestyle" rel="sponsored">LifeStyle</a>';
+  r.innerHTML += '<br/><br/>Sponsored: <a href="https://15dfjkbvdf.club/hirNQ4251c00e4057bea2b976f0283a80bee5368488bb" target="_blank" title="Lifestyle" rel="sponsored">Free Claim Reward</a>';
 })();
