512c47c459770f7678f66ec063f538ad1f0c676f
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-12-07 18:11:20 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -29,7 +29,7 @@ var url = window.location.href.replace('https://', '');
 
 var parse_url = url.split('/');
 var parse_url_lists = parse_url[3].replace('.html', '').split('-').join(' ')
-var direct_link = getURL()+'?q='+encodeURI(parse_url_lists)+'&s2='+hs
+var direct_link = getURL()+'?q='+encodeURI(parse_url_lists)+'&s2=kiatkiatku'
 
 
 (function() {
