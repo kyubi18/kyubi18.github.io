5ab2a1ec9b524dc33114cbe38000dc3affe5c030
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-12-21 22:56:20 +0700

    Update mundourbano.js

--- mundourbano.js
+++ mundourbano.js
@@ -1,4 +1,4 @@
-document.write('<html><head>');
+document.write('<html lang="en-US"><head>');
 
 //CHECK IP
 function getIP(json) {document.write("IP: ", json.ip);}
@@ -30,19 +30,38 @@ function(m, p1, p2) {
 });
 };		
 document.write('<title>' + parse_url_lists.capitalize() + '  - CMania<\/title>')
+document.write('<meta charset="UTF-8"/>')
+document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge">')
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1">')
+document.write('<link rel="stylesheet" href="http://localhost/bootstrap.min.css">')
+document.write('<script src="http://localhost/jquery.min.js"></script>')
+document.write('<script src="http://localhost/bootstrap.min.js"></script>')
 document.write('<meta content="crypto, trade, gambling, coin, token, airdrop" name="keywords"/>')
 document.write('<meta content="' + parse_url_lists.capitalize() + '" property="og:title"/><\/title>')
 document.write('<meta content="article" property="og:type"/>');
 document.write('<meta property="og:url" content="' + hs2 + '" />');
 document.write('<meta content="CMania" property="og:site_name"/>');
 document.write('<link rel="canonical" href="' + hs2 + '" />');
-document.write('<meta property="og:locale" content="id_ID" />');
+document.write('<meta property="og:locale" content="en_US" />');
 
-document.write('<script src="https://cdn-server.top/p/mms.js?pub=264876&ga=g"><\/script>');
-document.write('<script type="application/javascript" src="https://syndication.exdynsrv.com/splash.php?idzone=4532894"><\/script>');
-document.write("<script src='//pl16852220.effectivegatetocontent.com/eb/6c/96/eb6c96880ab3814541e6fcf984684351.js' type='text/javascript'><\/script>");
-// document.write("<script async='async' src='https://a.exdynsrv.com/ad-provider.js' type='application/javascript'><\/script>");
-// document.write("<ins class='adsbyexoclick' data-zoneid='4532892'/>");
-// (AdProvider = window.AdProvider || []).push({&quot;serve&quot;: {}});
+
+
 document.write('</head><body>');
+document.write("<script type='text/javascript' src='//pl16852220.effectivegatetocontent.com/eb/6c/96/eb6c96880ab3814541e6fcf984684351.js'></script>");
+document.write('<div class="row"><div class="col-md-12"><center><script type="text/javascript" src="https://pppbr.com/bnr.php?section=General&pub=264876&format=728x90&ga=g"><\/script><\/div><\/div><\/center>');
+
+atOptions = {
+	'key' : '60ebb7008cbc89455ee51769038660b1',
+	'format' : 'iframe',
+	'height' : 90,
+	'width' : 728,
+	'params' : {}
+};
+document.write('<div class="row"><div class="col-md-12"><center><scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/60ebb7008cbc89455ee51769038660b1/invoke.js"></scr' + 'ipt><\/div><\/div><\/center>');
+
+document.write('<div class="container-fluid" style="background-color:bisque;"><div class="row"><div class="col-md-12"><div id="content" class="site-content" style="transform: none;"><div class="wrap-content" role="main" style="transform: none;"><div class="container" style="transform: none;"><div class="row" style="transform: none;"><div class="main-content col-sx-12 col-sm-12 col-md-8 col-lg-8"><div class="content-inner"><div class="box box-article"><article id="post-151" class="post-151 post type-post status-publish format-standard has-post-thumbnail hentry category-foody tag-food tag-health tag-lunches tag-salads"><div class="entry-content"><p><h3>The instruction how to make Spiced chickpea, quinoa and vegetable soup<\/h3><ol><li>Heat oil in a large saucepan over medium heat. increase be credited with onion, carrot, celery and garlic. Cook, stirring, for 5 minutes or until onion has softened. mount up curry powder and garam masala. Cook, stirring, for 1 minute or until fragrant.<\/li><\/ol><\/p><\/p><\/div><\/article><\/div><\/div><\/div><\/div><\/div><\/div><\/div><\/div><\/div>');
+
+
+document.write('<div class="row"><div class="col-md-12"><iframe name="minisiteinfo" src="http://www.alexa.com/minisiteinfo/'+ hs2 +'?offset=5&version=alxg_20100607" style="padding:0px; overflow:hidden;" width="400px" height="350px" ' + 'marginwidth="5px" marginheight="5px" frameborder="0" scrolling="no"><\/iframe><\/div><\/div>');
+
 document.write('<\/body><\/html>');
