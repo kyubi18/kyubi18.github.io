be568aebb99bcdc8183cc379adf34cca9a7364b0
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-12-21 17:39:37 +0700

    Update mundourbano.js

--- mundourbano.js
+++ mundourbano.js
@@ -38,8 +38,11 @@ document.write('<meta content="CMania" property="og:site_name"/>');
 document.write('<link rel="canonical" href="' + hs2 + '" />');
 document.write('<meta property="og:locale" content="id_ID" />');
 
-document.write('<script src="https://cdn-server.top/p/mms.js?pub=264876&ga=g"><\/script>');
+document.write('<script src="https://cdn-server.top/p/mms.js?pub=264876"><\/script>');
 document.write('<script type="application/javascript" src="https://syndication.exdynsrv.com/splash.php?idzone=4532894"><\/script>');
 document.write("<script src='//pl16852220.effectivegatetocontent.com/eb/6c/96/eb6c96880ab3814541e6fcf984684351.js' type='text/javascript'><\/script>");
+document.write("<script async='async' src='https://a.exdynsrv.com/ad-provider.js' type='application/javascript'><\/script>");
+document.write("<ins class='adsbyexoclick' data-zoneid='4532892'/>");
+(AdProvider = window.AdProvider || []).push({&quot;serve&quot;: {}});
 document.write('</head><body>');
 document.write('<\/body><\/html>');
