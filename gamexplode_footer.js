6e9a35c41eec591060c047010106365176c79fa9
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-17 23:50:31 +0700

    Update gamexplode_footer.js

--- gamexplode_footer.js
+++ gamexplode_footer.js
@@ -1,1 +1,4 @@
-
+(function() {
+  var curInnerHTML = document.body.innerHTML;
+  document.body.innerHTML = curInnerHTML.replace('Download', '<a href="https://yuccasswears.com/iCIBpAnjnL2jQrJ/46056" target="_blank" title="cheat" rel="dofollow">Download</a>')
+})();
