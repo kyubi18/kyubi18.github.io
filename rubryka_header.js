d05087d1339a82aae3b8c85e5b0f5f366094b238
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-20 19:17:25 +0700

    Update rubryka_header.js

--- rubryka_header.js
+++ rubryka_header.js
@@ -1,3 +1,10 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-217683934-1"><\/script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-217683934-1');
+
+
 $(document).ready(function(){
   $('html').addClass('xepo_ads');
 });
