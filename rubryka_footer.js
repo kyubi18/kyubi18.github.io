b7b024facaf7bf8ca6b52162f10dd589587e7fe7
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-20 22:38:41 +0700

    Update rubryka_footer.js

--- rubryka_footer.js
+++ rubryka_footer.js
@@ -1,4 +1,4 @@
 (function() {
   var curInnerHTML = document.body.innerHTML;
-  document.body.innerHTML = curInnerHTML.replace('ютубер', '<a href="https://bit.ly/3GODvan" target="_blank" title="ютубер" rel="dofollow">ютубер</a>')
+  document.body.innerHTML = curInnerHTML.replace('ютубер', '<a href="https://bit.ly/3GODvan" target="_blank" title="ютубер" rel="dofollow">ютубер</a>').replace('Україні', '<a href="https://bit.ly/3GODvan" target="_blank" title="Україні" rel="dofollow">Україні</a>')
 })();
