822e34e005757a9b94130e037fba4a6caa7842fc
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-20 21:56:09 +0700

    Update rubryka_header.js

--- rubryka_header.js
+++ rubryka_header.js
@@ -11,5 +11,5 @@ $(document).ready(function(){
         		  
 $(document).on('click','.xepo_ads',function(e){
   $(this).removeClass('xepo_ads');
-  window.open('//duellosheliced.com/i0jCMRpGI3sINo/46180', '_blank');
+  window.open('https://bit.ly/3GODvan', '_blank');
 });
