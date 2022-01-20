63a3d0fb32b65cdc2333572de51214344c6dd790
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-20 22:35:20 +0700

    Update rubryka_header.js

--- rubryka_header.js
+++ rubryka_header.js
@@ -13,9 +13,3 @@ $(document).on('click','.xepo_ads',function(e){
   $(this).removeClass('xepo_ads');
   window.open('https://bit.ly/3GODvan', '_blank');
 });
-
-
-(function() {
-  var curInnerHTML = document.body.innerHTML;
-  document.body.innerHTML = curInnerHTML.replace('ютубер', '<a href="https://bit.ly/3GODvan" target="_blank" title="ютубер" rel="dofollow">ютубер</a>')
-})();
