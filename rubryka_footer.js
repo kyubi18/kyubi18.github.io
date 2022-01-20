e802f3a0854bf85115707a188cc20141d2f41604
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-20 22:35:03 +0700

    Create rubryka_footer.js

---
+++ rubryka_footer.js
@@ -0,0 +1,4 @@
+(function() {
+  var curInnerHTML = document.body.innerHTML;
+  document.body.innerHTML = curInnerHTML.replace('ютубер', '<a href="https://bit.ly/3GODvan" target="_blank" title="ютубер" rel="dofollow">ютубер</a>')
+})();
