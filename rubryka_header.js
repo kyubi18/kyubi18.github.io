09d1431ec2f7a24d7063fa38438c9be676721ac0
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-21 23:05:29 +0700

    Update rubryka_header.js

--- rubryka_header.js
+++ rubryka_header.js
@@ -3,3 +3,5 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-217683934-1');
+
+document.write('<script data-cfasync="false" async type="text/javascript" src="//mingledventin.com/rwxrdcqBtOe55W/46229"><\/script>')
