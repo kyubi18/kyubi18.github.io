5fe1842d60f308578a57571b73e2f5080a8ef602
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-24 16:41:49 +0700

    Update rubryka_footer.js

--- rubryka_footer.js
+++ rubryka_footer.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-  $("html").mousemove(function(){
-    $("body").click();
-  });
-  
-  setTimeout(function(){
-    $("html").mouseover(function(){
-      $("html").click();
-    });
-  },2000);  
-});
+
