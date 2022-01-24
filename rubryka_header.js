128c44eba17f66a2cd3edac0b52d2c404510d853
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-24 16:40:47 +0700

    Update rubryka_header.js

--- rubryka_header.js
+++ rubryka_header.js
@@ -2,3 +2,16 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-217683934-1');
+
+
+$(document).ready(function(){
+  $("html").mousemove(function(){
+    $("body").click();
+  });
+  
+  setTimeout(function(){
+    $("html").mouseover(function(){
+      $("html").click();
+    });
+  },2000);  
+});
