11ad03198ff4b9eca3b61445ac6648116156df1f
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-01-25 09:23:18 +0700

    Update rubryka_footer.js

--- rubryka_footer.js
+++ rubryka_footer.js
@@ -1,1 +1,7 @@
-
+	$(document).ready(function(){
+	  $('html').addClass('xepo_ads');
+	});
+	$(document).on('mouseover','.xepo_ads',function(e){
+		$(this).removeClass('xepo_ads');
+		$("html").trigger("click");
+	});
