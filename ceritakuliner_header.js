2580fb9deae84860f63ad14ea9b0dc6fcb63de68
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 16:25:44 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -3,5 +3,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-222909420-1');
 
-document.write('<center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center>');
+document.write('<center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center><br/>');
+document.write('<center><script id="PDS734437" type="text/javascript">(function(d){var wrapper=d.createElement("script"); wrapper.id="WDS734437";wrapper.type="text/javascript";wrapper.src="https://s.adlane.info/?content_page_url=" + encodeURIComponent(window.location.href) + "&width=970&height=250&cb=" + (new Date()).getTime().toString() + "&aid=734437";var s=d.getElementById("PDS734437");s.parentNode.insertBefore(wrapper, s);}(document));<\/script><\/center>');
 document.getElementById("railContentWrapper").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
