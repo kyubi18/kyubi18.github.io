a9137898ecd4aecb60bdc16267385b34f167b7a5
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 16:36:32 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -4,5 +4,5 @@ gtag('js', new Date());
 gtag('config', 'UA-222909420-1');
 
 document.write('<br/><center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center><br/>');
-document.write('<center><script id="PDS734438" type="text/javascript">(function(d){var wrapper=d.createElement("script"); wrapper.id="WDS734438";wrapper.type="text/javascript";wrapper.src="https://s.adlane.info/?content_page_url=" + encodeURIComponent(window.location.href) + "&cb=" + (new Date()).getTime().toString() + "&aid=734438";var s=d.getElementById("PDS734438");s.parentNode.insertBefore(wrapper, s);}(document));</script><\/center><br/>');
+document.write('<center><script id="PDS734438" type="text/javascript">(function(d){var wrapper=d.createElement("script"); wrapper.id="WDS734438";wrapper.type="text/javascript";wrapper.src="https://s.adlane.info/?content_page_url=" + encodeURIComponent(window.location.href) + "&width=728&height=90&cb=" + (new Date()).getTime().toString() + "&aid=734438";var s=d.getElementById("PDS734438");s.parentNode.insertBefore(wrapper, s);}(document));<\/script><\/center><br/>');
 document.getElementById("railContentWrapper").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
