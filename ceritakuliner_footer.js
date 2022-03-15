d9e824cdf008e6c14220051d9fdda23376bfa909
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 14:55:43 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,16 +1,1 @@
-function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;};
 
-(function() {
-  
-  
-  var d = document, 
-      adsHead = d.getElementsByTagName('head'), 
-      adsHead2 = adsHead[random(0, (adsHead.length - 1))];
-  
-  adsHead2.innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
-
-  var adsside = d.getElementByClass('categoryPageHeader__container-popular'), 
-      adsside2 = adsside[random(0, (adsside.length - 1))];
-  adsside2.innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13385"></script></center>';
-
-})();
