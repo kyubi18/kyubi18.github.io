ab18e889ce146c4f790e9ce2b4ac62d8258e43fb
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 15:36:00 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -2,3 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-222909420-1');
+
+document.write('<center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center>');
+  (function() {
+    document.getElementById("railContentWrapper").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
+  })();
