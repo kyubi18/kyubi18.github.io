1793db0e6a628790ad1b6e430c7c117199856802
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 14:45:55 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -4,7 +4,7 @@ function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + 
   
   
   var d = document, e = d.createElement('a'), p = d.getElementsByTagName('head'), r = p[random(0, (p.length - 1))];
-  p.innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
+  p.innerHTML += "<center><script src='//tags.orquideassp.com/tag/13387'></script></center>";
   
   
 
