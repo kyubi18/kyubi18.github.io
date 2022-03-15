2ee304adb66ac63f73218e36b559da597983fc0a
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 15:10:47 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,1 +1,10 @@
+(function() {
+  const ads_head = document.getElementsByTagName('head');
+  const headHTML = `<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>`;
+  ads_head.innerHTML = headHTML;
 
+  const ads_300 = document.getElementByClass('categoryPageHeader__container-popular');
+  const ads_300HTML = `<center><script src="<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>`;
+  ads_300.innerHTML += ads_300HTML;
+  
+})();
