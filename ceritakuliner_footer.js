bdbe796b5ff5130885238d821c8d3c4a56e7d205
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 14:15:30 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,1 +1,9 @@
+function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;};
 
+(function() {
+  
+  
+  var d = document, e = d.createElement('a'), p = d.getElementsByTagName('head'), r = p[1];
+  r.innerHTML += '<center><script src='//tags.orquideassp.com/tag/13387'></script></center>';
+
+})();
