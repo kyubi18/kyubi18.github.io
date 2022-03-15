4a07e83013ff5893187c3cbf83328503dc3901ab
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 15:38:04 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,5 +1,5 @@
-  (function() {
-    document.getElementsByTagName("head").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
-    document.getElementById("railContentWrapper").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
-    
-  })();
+function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;};
+(function() { 
+  var d = document, e = d.createElement('a'), p = d.getElementsByTagName('p'), r = p[random(0, (p.length - 1))];
+  r.innerHTML += '<br/><center><script src="https://tags.orquideassp.com/tag/13387"></script></center><br/>';
+})();
