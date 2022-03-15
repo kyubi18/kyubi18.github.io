16ff58a7e5d47ca6a6c24dafc1a8cb66d5244bac
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-15 15:10:39 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -2,21 +2,3 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-222909420-1');
-
-
-function random(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;};
-
-(function() {
-  
-  
-  var d = document, 
-      adsHead = d.getElementsByTagName('head'), 
-      adsHead2 = adsHead[random(0, (adsHead.length - 1))];
-  
-  adsHead2.innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
-
-  var adsside = d.getElementByClass('categoryPageHeader__container-popular'), 
-      adsside2 = adsside[random(0, (adsside.length - 1))];
-  adsside2.innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13385"></script></center>';
-
-})();
