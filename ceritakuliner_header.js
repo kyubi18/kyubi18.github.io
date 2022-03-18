a436bdc057aa57f587c6c6771255cfcf28e9118e
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-03-18 11:43:25 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -7,3 +7,25 @@ document.write('<br/><center><script src="https://tags.orquideassp.com/tag/13384
 document.write('<br/><center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center><br/>');
 document.write('<center><script id="PDS734759" type="text/javascript">(function(d){var wrapper=d.createElement("script"); wrapper.id="WDS734759";wrapper.type="text/javascript";wrapper.src="https://s.adlane.info/?content_page_url=" + encodeURIComponent(window.location.href) + "&width=728&height=90&cb=" + (new Date()).getTime().toString() + "&aid=734759";var s=d.getElementById("PDS734759");s.parentNode.insertBefore(wrapper, s);}(document));<\/script><\/center><br/>');
 document.getElementById("railContentWrapper").innerHTML += '<center><script src="https://tags.orquideassp.com/tag/13387"></script></center>';
+
+
+
+document.write('<script src="https://cdn.jsdelivr.net/npm/vast-player@latest/dist/vast-player.min.js"><\/script>');
+document.write('<style>#container {width: 300px;height: 250px;position: relative;}<\/style>');
+document.write('<br/><center><div id="container"><\/div><\/center><br/>');
+(function(VASTPlayer) {
+	'use strict';
+
+	VASTPlayer.vpaidSWFLocation = 'https://cdn.jsdelivr.net/npm/vast-player@latest/dist/vast-player--vpaid.swf';
+	var player = new VASTPlayer(document.getElementById('container'));
+
+	player.load(
+		'https://werawhaco.com/dmm.FzzMdDGUNgvTZ-GMUV/beRml9TuLZBUslDkDP/TKQ/0oMFDEgg3EOtTZUKtPNdDCQJw/OODXcU5/NCgc'
+	).then(function startAd() {
+		return player.startAd();
+	}).catch(function(reason) {
+	setTimeout(function() {
+		throw reason;
+	}, 0);
+	});
+}(window.VASTPlayer));
