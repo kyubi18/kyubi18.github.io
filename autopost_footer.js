d5dd390eac4b986c849620e0d78b0fd497880875
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 12:01:08 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,7 +1,7 @@
 document.write("<script type='text/javascript' src='//pl16869861.trustedcpmrevenue.com/5c/86/6d/5c866d5ed643fde8ff9889df0751d67c.js'><\/script>");
 
 var d = document, p = d.getElementsByTagName('body'), r = p[0];
-r.innerHTML += '<div id="fixedban" style="position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;"><div><a id="close-fixedban" onclick="document.getElementById(&apos;fixedban&apos;).style.display=&apos;none&apos;;" style="cursor:pointer;"><img alt="close" src="http://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif" title="close button" style="vertical-align:middle;"/></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto"><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img style="max-width:100%;height:auto;vertical-align:middle;" alt="MiniLemon" src="https://kyubi18.github.io/minilemon300.jpg"/></a></div></div>';
+r.innerHTML += '<div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a><div class="overlay-content"><center><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img src="https://kyubi18.github.io/minilemon300.jpg" alt="MiniLemon"></a></center></div></div>';
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
