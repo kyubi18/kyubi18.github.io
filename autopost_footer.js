8f949ba708626d4a72a6bc4b2c51d2b37b1eea93
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 11:39:04 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,13 +1,2 @@
-document.write('<style>.overlay{height: 100%; width: 0; position: absolute; z-index: 1; top: 0; left: 0; background-color: rgb(0,0,0); background-color: rgba(0,0,0, 0.9); overflow-x: hidden; transition: 0.5s;}.overlay-content{position: absolute; top: 25%; width: 100%; text-align: center; margin-top: 30px;}.overlay a{padding: 8px; text-decoration: none; font-size: 36px; color: #818181; display: block; transition: 0.3s;}.overlay a:hover, .overlay a:focus{color: #f1f1f1;}.closebtn{position: absolute; top: 20px; right: 45px; font-size: 60px !important;}@media screen and (max-height: 450px){.overlay a{font-size: 20px}.closebtn{font-size: 40px !important; top: 15px; right: 35px;}}<\/style><div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×<\/a><div class="overlay-content"> <center><a href="https:\/\/minilemon.co.id"><img src="https:\/\/kyubi18.github.io\/minilemon300.jpg" alt="MiniLemon"><\/a<\/center> <\/div><\/div>');
-
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
-openNav();
-
-
+document.write('<div id="fixedban" style="position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;"><div><a id="close-fixedban" onclick="document.getElementById(&apos;fixedban&apos;).style.display=&apos;none&apos;;" style="cursor:pointer;"><img alt="close" src="http://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif" title="close button" style="vertical-align:middle;"/><\/a><\/div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto"><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img style="max-width:100%;height:auto;vertical-align:middle;" alt="MiniLemon" src="https://kyubi18.github.io/minilemon300.jpg"/><\/a><\/div><\/div>');
 document.write("<script type='text/javascript' src='//pl16869861.trustedcpmrevenue.com/5c/86/6d/5c866d5ed643fde8ff9889df0751d67c.js'><\/script>");
