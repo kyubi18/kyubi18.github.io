57a7d2973d06bd8a9735140e371f4f164e2f123e
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 12:03:45 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,6 +1,6 @@
 document.write("<script type='text/javascript' src='//pl16869861.trustedcpmrevenue.com/5c/86/6d/5c866d5ed643fde8ff9889df0751d67c.js'><\/script>");
 
-var d = document, p = d.getElementsByTagName('body'), r = p[0];
+var d = document, p = d.getElementsByTagName('html'), r = p[0];
 r.innerHTML += '<div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a><div class="overlay-content"><center><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img src="https://kyubi18.github.io/minilemon300.jpg" alt="MiniLemon"></a></center></div></div>';
 
 function openNav() {
