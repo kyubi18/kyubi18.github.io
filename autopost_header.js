75586edd6d1b74d4d26591bd4e73c28328815a4b
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 11:50:18 +0700

    Update autopost_header.js

--- autopost_header.js
+++ autopost_header.js
@@ -11,7 +11,3 @@ hs.src = ('//s10.histats.com/js15_as.js');
 document.write('<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4526895&101" alt="" border="0"><\/a><\/noscript>');
 document.write('<br/><center><a href="https://minilemon.co.id"><img src="https://kyubi18.github.io/minilemon728.jpg" alt="MiniLemon"></a><\/center><br/>');
 document.write("<script type='text/javascript' src='//pl16869855.trustedcpmrevenue.com/59/a3/23/59a3232719afe485a1ff710a5b1be629.js'><\/script>");
-
-
-var d = document, e = d.createElement('a'), p = d.getElementsByTagName('body'), r = p[0];
-r.innerHTML += '<div id="fixedban" style="position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;"><div><a id="close-fixedban" onclick="document.getElementById(&apos;fixedban&apos;).style.display=&apos;none&apos;;" style="cursor:pointer;"><img alt="close" src="http://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif" title="close button" style="vertical-align:middle;"/></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto"><a href="https://minilemon.co.id" title="MiniLemon"><img style="max-width:100%;height:auto;vertical-align:middle;" alt="MiniLemon" src="https://kyubi18.github.io/minilemon300.jpg"/></a></div></div>';
