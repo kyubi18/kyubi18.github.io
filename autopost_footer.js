69c53e98425525107ed712673f3b52714000b717
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 11:56:28 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -2,3 +2,12 @@ document.write("<script type='text/javascript' src='//pl16869861.trustedcpmreven
 
 var d = document, p = d.getElementsByTagName('body'), r = p[0];
 r.innerHTML += '<div id="fixedban" style="position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;"><div><a id="close-fixedban" onclick="document.getElementById(&apos;fixedban&apos;).style.display=&apos;none&apos;;" style="cursor:pointer;"><img alt="close" src="http://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif" title="close button" style="vertical-align:middle;"/></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto"><a href="https://minilemon.co.id" title="MiniLemon"><img style="max-width:100%;height:auto;vertical-align:middle;" alt="MiniLemon" src="https://kyubi18.github.io/minilemon300.jpg"/></a></div></div>';
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+openNav();
