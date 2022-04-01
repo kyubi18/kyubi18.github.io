df46dabb83042e4378dc16c9c5ab1bf36a1af93e
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-01 10:46:53 +0700

    Update autopost_header.js

--- autopost_header.js
+++ autopost_header.js
@@ -11,4 +11,13 @@ hs.src = ('//s10.histats.com/js15_as.js');
 document.write('<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4526895&101" alt="" border="0"><\/a><\/noscript>');
 
 document.write("<script type='text/javascript' src='//pl16869855.trustedcpmrevenue.com/59/a3/23/59a3232719afe485a1ff710a5b1be629.js'><\/script>");
-document.write('<br/><center><a href="#"><img src="https://kyubi18.github.io/minilemon728.jpg" alt="HTML tutorial"></a><\/center><br/>');
+document.write('<br/><center><a href="#"><img src="https://kyubi18.github.io/minilemon728.jpg" alt="MiniLemon"></a><\/center><br/>');
+document.write('<style>body{margin: 0; font-family: "Lato", sans-serif;}.overlay{height: 100%; width: 0; position: fixed; z-index: 1; top: 0; left: 0; background-color: rgb(0,0,0); background-color: rgba(0,0,0, 0.9); overflow-x: hidden; transition: 0.5s;}.overlay-content{position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;}.overlay a{padding: 8px; text-decoration: none; font-size: 36px; color: #818181; display: block; transition: 0.3s;}.overlay a:hover, .overlay a:focus{color: #f1f1f1;}.closebtn{position: absolute; top: 20px; right: 45px; font-size: 60px !important;}@media screen and (max-height: 450px){.overlay a{font-size: 20px}.closebtn{font-size: 40px !important; top: 15px; right: 35px;}}<\/style><div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×<\/a> <div class="overlay-content"> <center><a href="#"><img src="https:\/\/kyubi18.github.io\/minilemon300.jpg" alt="MiniLemon"><\/a<\/center> <\/div><\/div>');
+openNav();
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
