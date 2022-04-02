c6d21e43d03da4423142a18d93bdf97fe0d851a2
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-02 15:06:30 +0700

    Update ceritakuliner_header.js

--- ceritakuliner_header.js
+++ ceritakuliner_header.js
@@ -6,6 +6,3 @@ gtag('config', 'UA-222909420-1');
 document.write('<center><iframe src="https://viewm.moonicorn.network/#%7B%22options%22%3A%7B%22publisherAddr%22%3A%220x2520942392fc918DB01f4057a48A0A814CBA7941%22%2C%22whitelistedTokens%22%3A%5B%220x6B175474E89094C44Da98b954EedeAC495271d0F%22%5D%2C%22whitelistedType%22%3A%22legacy_728x90%22%2C%22randomize%22%3Atrue%2C%22targeting%22%3A%5B%5D%2C%22width%22%3A%22728%22%2C%22height%22%3A%2290%22%2C%22minPerImpression%22%3A%220%22%2C%22fallbackUnit%22%3Anull%2C%22marketSlot%22%3A%22QmPoG35a1yAP7K9fniwxYbCUV25tZXEDU7eJ1QbfaJNBhZ%22%7D%7D" width="728" height="90" scrolling="no" frameborder="0" style="border: 0;"><\/iframe><\/center><br/>');
 document.write('<center><script src="https://tags.orquideassp.com/tag/13384"><\/script><\/center><br/>');
 document.write('<center><script src="https://tags.orquideassp.com/tag/13387"><\/script><\/center>');
-
-
-$(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#bl_banner&#39;).hide(90);});});
