ec90251635ffac2ff630b4c6c4d7fa7006582498
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-02 17:24:08 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,2 +1,2 @@
-var d = document, p = d.getElementsByTagName('head'), r = p[1];
+var d = document, p = d.getElementsByTagName('html'), r = p[1];
 r.innerHTML += '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto"><iframe width="300" height="250" src="//cdn.tubecorp.com/i/b.html?spot=11061&src=601129671&pid=33603&width=300&height=250&spaceid=916" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div></div>';
