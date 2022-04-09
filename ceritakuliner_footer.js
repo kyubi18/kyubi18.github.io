4998ddbf7779139999b537ef32a687e1bcb268aa
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-04-09 13:03:41 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,3 +1,5 @@
 document.write('<script type="text/javascript" src="//pl16869861.safestgatetocontent.com/5c/86/6d/5c866d5ed643fde8ff9889df0751d67c.js"><\/script>');
+document.write('<script type="text/javascript" src="https://player.adlane.info/static/adlane-push/atpns.core.js" data-atpns-spot-id="18034" data-atpns-sw="/atpns.sw.js" defer="true" async="true"><\/script>');
+
 var d = document, p = d.getElementsByTagName('html'), r = p[0];
 r.innerHTML += '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto"><iframe width="300" height="250" src="//cdn.tubecorp.com/i/b.html?spot=11061&src=601129671&pid=33603&width=300&height=250&spaceid=916" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div></div>';
