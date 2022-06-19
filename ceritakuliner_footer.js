d7589059db6fb7b7e22737270177c111b7b47c07
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-06-19 14:52:06 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -1,10 +1,10 @@
 document.write('<script type="text/javascript" src="//pl16869861.safestgatetocontent.com/5c/86/6d/5c866d5ed643fde8ff9889df0751d67c.js"><\/script>');
 document.write('<script src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"><\/script>');
-var tags_1 = '/211182487,22679029955/www.ceritakuliner.com_Display_300x250';
-var sizes_1 = [[320,480],[640,480],[300,250],[300,600]];
+
+
 window.googletag = window.googletag || {cmd: []};
 googletag.cmd.push(function() {				
-	googletag.defineSlot(tags_1, sizes_1, 'banner-1').addService(googletag.pubads());
+	googletag.defineSlot('/211182487,22679029955/www.ceritakuliner.com_Display_300x250', [[320,480],[640,480],[300,250],[300,600]], 'banner-1').addService(googletag.pubads());
 
 	googletag.pubads().collapseEmptyDivs();
 	googletag.pubads().setCentering(true);
@@ -15,4 +15,4 @@ googletag.cmd.push(function() {
 	
 	
 var d = document, p = d.getElementsByTagName('html'), r = p[0];
-r.innerHTML += '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto"><center><div id="banner-1"><script>googletag.cmd.push(function() { googletag.display("banner-1"); });</script></div></center></div></div>';
+r.innerHTML += '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto"><div id="banner-1"><script>googletag.cmd.push(function() { googletag.display("banner-1"); });</script></div></div></div>';
