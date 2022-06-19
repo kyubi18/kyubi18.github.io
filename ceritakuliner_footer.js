46d86b23fd5fae0b14b373378db6861769401772
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-06-19 15:25:32 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -2,17 +2,23 @@ document.write('<script type="text/javascript" src="//pl16869861.safestgatetocon
 document.write('<script src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"><\/script>');
 
 
-window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function() {				
-	googletag.defineSlot('/211182487,22679029955/www.ceritakuliner.com_Display_300x250', [[320,480],[640,480],[300,250],[300,600]], 'banner-1').addService(googletag.pubads());
+window.googletag = window.googletag || {
+    cmd: []
+};
+googletag.cmd.push(function() {
+    googletag.defineSlot('/211182487,22679029955/www.ceritakuliner.com_Display_300x250', [
+        [320, 480],
+        [640, 480],
+        [300, 250],
+        [300, 600]
+    ], 'banner-1').addService(googletag.pubads());
 
-	googletag.pubads().collapseEmptyDivs();
-	googletag.pubads().setCentering(true);
-	googletag.pubads().setTargeting('finance','health','food','sports', 'movies');
-	googletag.pubads().enableSingleRequest();
-	googletag.enableServices();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.pubads().setCentering(true);
+    googletag.pubads().setTargeting('finance', 'health', 'food', 'sports', 'movies');
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
 });
-	
-	
-var d = document, p = d.getElementsByTagName('html'), r = p[0];
-r.innerHTML += '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto"><div id="banner-1"><script>googletag.cmd.push(function() { googletag.display("banner-1"); });</script></div></div></div>';
+
+
+document.write('<center><div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:99999;-webkit-transform:translateZ(0);"><div style="text-align:right;display:block;max-width:640px;height:640px;overflow:hidden;margin:auto"><div id="banner-1"><script>googletag.cmd.push(function(){googletag.display("banner-1");});<\/script><\/div><\/div><\/div><\/center>');
