39c79d171dacdab6dba641c6003100bd8a8e950c
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-06-24 11:27:13 +0700

    Update ceritakuliner_footer.js

--- ceritakuliner_footer.js
+++ ceritakuliner_footer.js
@@ -21,4 +21,4 @@ googletag.cmd.push(function() {
 });
 
 document.write('<div id="floatads" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:100px;z-index:99999;-webkit-transform:translateZ(0);"><div><a id="close-floatads" onclick="document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;" style="cursor:pointer;"><img alt="close" src="https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif" title="close button"/></a></div><div style="text-align:center;display:block;max-width:640px;height:auto;overflow:hidden;margin:auto"><div id="banner-1"><script>googletag.cmd.push(function(){googletag.display("banner-1");});<\/script><\/div></div></div>');
-document.write('<script src='//tags.orquideassp.com/tag/18103'><\/script>');
+document.write('<script src="//tags.orquideassp.com/tag/18103"><\/script>');
