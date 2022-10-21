079c3bffddc73835666f0b93f62ed4ffb3fc5de4
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-10-21 08:41:25 +0700

    Update brainly_footer.js

--- brainly_footer.js
+++ brainly_footer.js
@@ -7,3 +7,14 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
+
+atOptions = {
+	'key' : '0917daf89c8455ca89b3c337d864a9c6',
+	'format' : 'iframe',
+	'height' : 90,
+	'width' : 728,
+	'params' : {}
+};
+document.write('<center>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://compensationpropulsion.com/0917daf89c8455ca89b3c337d864a9c6/invoke.js"></scr' + 'ipt>');
+document.write('<\/center>');
