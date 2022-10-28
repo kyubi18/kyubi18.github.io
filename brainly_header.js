c8f675d53b8994658d832f5b2704413537edbc56
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-10-28 22:18:13 +0700

    Update brainly_header.js

--- brainly_header.js
+++ brainly_header.js
@@ -7,4 +7,8 @@ atOptions = {
 };
 document.write('<center>');
 document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://compensationpropulsion.com/837001a55ca2947c0eb5a6c3b4b4adb3/invoke.js"></scr' + 'ipt>');
+
+document.write('<script async="async" data-cfasync="false" src="//compensationpropulsion.com/c3c8121b4c55ed9bd80d12bb7dd5e768/invoke.js"><\/script>');
+document.write('<div id="container-c3c8121b4c55ed9bd80d12bb7dd5e768"><\/div>');
+
 document.write('<\/center>');
