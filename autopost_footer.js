da4a84c08ac7b9f5b00cdfdbc3d83e221d44e6a9
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2022-10-31 07:39:49 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,3 +1,6 @@
+document.write("<script type='text/javascript' src='//pl17178977.profitablegatetocontent.com/c7/ee/f9/c7eef932a03cdc24428e7940fc48680d.js'><\/script>");
+document.write("<script type='text/javascript' src='//pl17178979.profitablegatetocontent.com/79/65/06/79650682d2e3cadac986a94bd0d4ec55.js'><\/script>");
+
 var d = document, p = d.getElementsByTagName('html'), r = p[0];
 r.innerHTML += '<div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a><div class="overlay-content"><center><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img src="https://kyubi18.github.io/minilemon300.jpg" alt="MiniLemon"></a></center></div></div>';
 
@@ -9,7 +12,3 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 openNav();
-
-
-document.write("<script type='text/javascript' src='//pl17178977.profitablegatetocontent.com/c7/ee/f9/c7eef932a03cdc24428e7940fc48680d.js'><\/script>");
-document.write("<script type='text/javascript' src='//pl17178979.profitablegatetocontent.com/79/65/06/79650682d2e3cadac986a94bd0d4ec55.js'><\/script>");
