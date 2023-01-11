ffc8a8d0a2be7a6469c74fb54c9145efbe7bacd7
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2023-01-11 18:34:27 +0700

    Update ceritakuliner_footerX.js

--- ceritakuliner_footerX.js
+++ ceritakuliner_footerX.js
@@ -1,2 +1,1 @@
-document.write("<script type='text/javascript' src='//compensationpropulsion.com/48/88/40/488840ec36015865886cbcd513cd8f97.js'><\/script>");
-document.write("<script type='text/javascript' src='//compensationpropulsion.com/a6/77/63/a677639da6619d2a80b3f93cfb4675f4.js'><\/script>");
+
