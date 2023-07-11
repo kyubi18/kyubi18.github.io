e2cee4afbf2980452952b38389d2cfcfb297dbd0
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2023-07-11 18:44:38 +0700

    Update autopost_header.js

--- autopost_header.js
+++ autopost_header.js
@@ -7,16 +7,8 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
-document.write("<script type='text/javascript' src='//pl17178977.profitablegatetocontent.com/c7/ee/f9/c7eef932a03cdc24428e7940fc48680d.js'><\/script>");
+document.write("<script type='text/javascript' src='//pl20014110.highrevenuegate.com/bb/fd/99/bbfd9922fe91c5d18b043e992f3b6a39.js'><\/script>");
 document.write('<style>.overlay{height: 100%; width: 0; position: fixed; z-index: 999999999999999999999999999999; top: 0; left: 0; background-color: rgb(0,0,0); background-color: rgba(0,0,0, 0.95); overflow-x: hidden; transition: 0.5s;}.overlay-content{position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;}.overlay a{padding: 8px; text-decoration: none; font-size: 36px; color: #818181; display: block; transition: 0.3s;}.overlay a:hover, .overlay a:focus{color: #f1f1f1;}.closebtn{position: absolute; top: 20px; right: 45px; font-size: 60px !important;}@media screen and (max-height: 450px){.overlay a{font-size: 20px}.closebtn{font-size: 40px !important; top: 15px; right: 35px;}}<\/style>');
 
 document.write('<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4526895&101" alt="" border="0"><\/a><\/noscript>');
 document.write('<br\/><center><a href="https://minilemon.co.id" target="_blank"><img src="https://kyubi18.github.io/minilemon728.jpg" alt="MiniLemon"></a><\/center><br\/>');
-atOptions = {
-	'key' : '603e962944fda02de79cb229525db591',
-	'format' : 'iframe',
-	'height' : 90,
-	'width' : 728,
-	'params' : {}
-};
-document.write('<br\/><center><scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/603e962944fda02de79cb229525db591/invoke.js"></scr' + 'ipt><\/center><br\/>');
