6a52ebfd9e078c2fd99dfcdb4fae943fa8511908
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2023-07-11 18:43:47 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,5 +1,4 @@
-document.write("<script type='text/javascript' src='//pl17178979.highperformancecpmgate.com/79/65/06/79650682d2e3cadac986a94bd0d4ec55.js'><\/script>");
-document.write("<script async src='https://js.wpadmngr.com/static/adManager.js' data-admpid='71593'><\/script>");
+document.write("<script type='text/javascript' src='//pl20014460.highrevenuegate.com/81/5e/a1/815ea10f7b212248b01f3a7e53f204e9.js'><\/script>");
 
 var d = document, p = d.getElementsByTagName('html'), r = p[0];
 r.innerHTML += '<div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a><div class="overlay-content"><center><a href="https://minilemon.co.id" title="MiniLemon" target="_blank"><img src="https://kyubi18.github.io/minilemon300.jpg" alt="MiniLemon"></a></center></div></div>';
