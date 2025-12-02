499db084dc03b76f1046d138c51db185344eb122
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2025-12-02 23:12:22 +0700

    Update autopost_header.js

--- autopost_header.js
+++ autopost_header.js
@@ -11,4 +11,6 @@ document.write("<script type='text/javascript' src='//pl20014110.highrevenuegate
 document.write('<style>.overlay{height: 100%; width: 0; position: fixed; z-index: 999999999999999999999999999999; top: 0; left: 0; background-color: rgb(0,0,0); background-color: rgba(0,0,0, 0.95); overflow-x: hidden; transition: 0.5s;}.overlay-content{position: relative; top: 25%; width: 100%; text-align: center; margin-top: 30px;}.overlay a{padding: 8px; text-decoration: none; font-size: 36px; color: #818181; display: block; transition: 0.3s;}.overlay a:hover, .overlay a:focus{color: #f1f1f1;}.closebtn{position: absolute; top: 20px; right: 45px; font-size: 60px !important;}@media screen and (max-height: 450px){.overlay a{font-size: 20px}.closebtn{font-size: 40px !important; top: 15px; right: 35px;}}<\/style>');
 
 document.write('<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4526895&101" alt="" border="0"><\/a><\/noscript>');
-document.write('<br\/><center><a href="https://minilemon.co.id" target="_blank"><img src="https://kyubi18.github.io/minilemon728.jpg" alt="MiniLemon"></a><\/center><br\/>');
+
+atOptions = {'key' : '56a53fe1b516182ec510a5fef87730f4', 'format' : 'iframe', 'height' : 90, 'width' : 728, 'params' : {}}
+document.write('<br\/><center><script type='text/javascript' src='//www.highperformanceformat.com/56a53fe1b516182ec510a5fef87730f4/invoke.js'><\/script><\/center><br\/>');
