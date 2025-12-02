b6e295354ad37c962af6daa6fd969b89b2ae63b2
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2025-12-02 23:24:47 +0700

    Update autopost_footer.js

--- autopost_footer.js
+++ autopost_footer.js
@@ -1,15 +1,1 @@
-document.write("<script type='text/javascript' src='https://js.wpadmngr.com/static/adManager.js' data-admpid='398361'><\/script>");
-document.write("<script type='text/javascript' src='//pl28176871.effectivegatecpm.com/d6/db/90/d6db9090613bcff091c8c46b7775ca7f.js'><\/script>");
-document.write("<script type='text/javascript' async='async' data-cfasync='false' src='//pl28176894.effectivegatecpm.com/641208a028e5cbce37a0b8fc50ec9aea/invoke.js'><\/script>");
 
-var d = document, p = d.getElementsByTagName('html'), r = p[0];
-r.innerHTML += '<div id="myNav" class="overlay"> <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a><div class="overlay-content"><center><div id="container-641208a028e5cbce37a0b8fc50ec9aea"></div></center></div></div>';
-
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
-openNav();
